1657067864ff28f4d50568bde41ab10cf27f560d
Thanh Vu <vu.nht00@gmail.com>
2022-03-11 10:39:01 +0700

    change localtion of App file to app folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from '../features/Footer/Footer';
+import Navbar from '../features/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Boring website</h1>
+      <Footer/>
     </div>
   );
 }
