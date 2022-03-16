9257f9cb4bdf083a5d152b71df8913d07b92746f
Thanh Vu <vu.nht00@gmail.com>
2022-03-16 09:44:40 +0700

    make basic structure for app index and search

--- src/components/Index/AppIndex.js
+++ src/components/Index/AppIndex.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Reddit } from "../../utility/reddit";
 import Filter from "../../features/Filter/Filter";
+import Search from "../../features/Search/Search";
 
 const AppIndex = () => {
+
   return (
     <main id="app-index">
       <Filter/>
+      <Search/>
+      <button className="btn btn-outline-info" onClick={Reddit.authorization}>
+        Click here
+      </button>
+      <button className="btn btn-outline-info" onClick={Reddit.getAccessToken}>
+        Get Access Token
+      </button>
     </main>
   )
 }
---
+++ src/features/Search/Search.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Search = () => {
+  const {term, setTerm} = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <section id="search" className="container-fluid" onSubmit={handleSubmit}>
+      <form>
+        <input type="text" value={term} onChange={(e) => {
+          setTerm(e.target.value)
+        }} />
+        <input type="submit"/>
+      </form>
+    </section>
+  )
+}
+
+export default Search;
