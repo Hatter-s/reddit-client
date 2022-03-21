711c59135defa9c54c7f7a5d13f702780b417946
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-03-21 20:19:45 +0700

    add footer

--- src/features/Footer/Footer.js
+++ src/features/Footer/Footer.js
@@ -1,39 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './footer.css'
 
 const Footer = () => {
   return (
     <footer className="container-fuild">
-      <section id='creator'>
-        <p className="lead">This project make by:</p>        
-        <div id='gia-thanh'>
-          <p>Nguyen Gia Thanh</p>
-          <div className="icon-container">
-            <a href='/' target='_blank'><i className="bi bi-facebook"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-twitter"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-inbox"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-github"></i></a>
-          </div>
+      <div id="footer">
+        <figure id="footer-icon" >
+            <img  src="https://cdn-icons-png.flaticon.com/512/52/52053.png"/>
+            <p id="footer-name">Reddit</p>
+        </figure>
+        <section id="footer-menu">
+            <Link className='footer-link' to="/">About</Link>
+            <Link className='footer-link' to="/">Terms</Link>
+            <Link className='footer-link' to="/">User Help Community</Link>
+        </section>
+        <section id="footer-team">
+            <Link className='footer-link' id="team-name" to="/">GTV Team</Link>
+            <p>Email:abcxyz@gmail.com</p>
+            <p>Phone:0912398293</p>
+            <p>Address:178 Quan Nhan, Ha Noi, Viet Nam</p>
+        </section>
         </div>
-        <div id='thanh-vu'>
-          <p>Nguyen Ha Thanh Vu</p>
-          <div className="icon-container">
-            <a href='/' target='_blank'><i className="bi bi-facebook"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-twitter"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-inbox"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-github"></i></a>
-          </div>
-        </div>
-        
-      </section>
-      <section id='infomation'>
-        <p className="lead">More infomation about this project</p>
-        <div className="icon-container">
-            <a href='/' target='_blank'><i className="bi bi-file-pdf"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-file-text"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-inbox"></i></a>
-            <a href='/' target='_blank'><i className="bi bi-github"></i></a>
-          </div>
-      </section>
     </footer>
   )
 }
