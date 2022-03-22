7c1ff263dc824d4a2fe4ae39802dfa9ed15c0375
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-03-22 10:54:07 +0700

    add navbar

---
+++ src/features/Navbar/navbar1.js
@@ -0,0 +1,20 @@
+{/* <button  
+          className='navbar-toggler ms-auto' 
+          type='button' 
+          data-bs-toggle='collapse' 
+          data-bs-target='#navbar-collapse'>
+          <span className='navbar-toggler-icon'></span>
+        </button>*/}
+{/* <div className='collapse navbar-collapse' id='navbar-collapse'>
+          <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
+            <button type='button' class="btn btn-light" id="coin">Get Coins</button>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>Log in</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>Sign up</Link>
+            </li>
+          </ul>
+        </div>  */}
