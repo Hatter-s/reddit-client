4516c74c49d60dfd8245c3499e345b94f11f0270
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-03-30 11:27:42 +0700

    update navbar
    update last version

--- src/components/Index/AppIndex.js
+++ src/components/Index/AppIndex.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Reddit } from "../../utility/reddit";
 import Filter from "../../features/Filter/Filter";
-import Search from "../../features/Search/Search";
+
 
 const AppIndex = () => {
 
   return (
     <main id="app-index">
       <Filter/>
-      <Search/>
+      
       <button className="btn btn-outline-info" onClick={Reddit.authorization}>
         Click here
       </button>
