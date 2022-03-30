23fe063128f524ccf5d2a8f526ec18c7241bd98d
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-03-30 11:32:55 +0700

    update contact
    change MUI to bootstrap

--- src/features/ContactUs/contactUs.js
+++ src/features/ContactUs/contactUs.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import PersonIcon from '@mui/icons-material/Person';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import BusinessIcon from '@mui/icons-material/Business';
 import './contact_us.css'
 
 export class ContactHeader extends React.Component{
@@ -25,19 +21,19 @@ export class Information extends React.Component{
         return (
             <div id="information">
                 <section id='person'>
-                    <PersonIcon></PersonIcon>
+                    <i class="bi bi-person-fill"></i>
                     <p>Mr.Thanh</p>
                 </section>
                 <section id='phone'>
-                    <LocalPhoneIcon></LocalPhoneIcon>
+                    <i class="bi bi-telephone-fill"></i>
                     <p>+84 921852309</p>
                 </section>
                 <section id='email'>
-                    <AlternateEmailIcon></AlternateEmailIcon>
+                    <i class="bi bi-envelope-fill"></i>
                     <p>thanhHH1907@gmail.com</p>
                 </section>
                 <section id='address'>
-                    <BusinessIcon></BusinessIcon>
+                    <i class="bi bi-door-open-fill"></i>
                     <p>178 Quan Nhan, Thanh Xuan , Ha Noi , VietNam</p>
                 </section>
         </div>
