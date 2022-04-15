15a223bf1eb576ca0bc849f1d732da26132f9afe
Thanh Vu <vu.nht00@gmail.com>
2022-04-15 11:44:22 +0700

    modify and update UI for Footer and Navbar

--- src/features/Footer/Footer.js
+++ src/features/Footer/Footer.js
@@ -6,22 +6,24 @@ const Footer = () => {
   return (
     <footer className="container-fuild">
       <div id="footer">
-        <figure id="footer-icon" >
+        <figure id="contain-footer-icon" >
             <img  src="https://cdn-icons-png.flaticon.com/512/52/52053.png" alt=""/>
-            <p id="footer-name">Reddit</p>
+            <figcaption id="footer-name">Reddit</figcaption>
         </figure>
+
         <section id="footer-menu">
             <Link className='footer-link' to="/">About</Link>
             <Link className='footer-link' to="/">Terms</Link>
             <Link className='footer-link' to="/">User Help Community</Link>
         </section>
+
         <section id="footer-team">
             <Link className='footer-link' id="team-name" to="/">GTV Team</Link>
             <p>Email:abcxyz@gmail.com</p>
             <p>Phone:0912398293</p>
             <p>Address:178 Quan Nhan, Ha Noi, Viet Nam</p>
         </section>
-        </div>
+      </div>
     </footer>
   )
 }
--- src/features/Navbar/Navbar.js
+++ src/features/Navbar/Navbar.js
@@ -22,26 +22,33 @@ const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='container-fluid'>
-        <Link className='navbar-brand' to='/'><img id ="nav-icon" src="https://cdn-icons-png.flaticon.com/512/52/52053.png" alt=""/></Link>
-        <button type='button' class="btn btn-light" id="nav-home">Home</button>
-        <form id='nav-search' class="form-inline my-2 my-lg-0">
-          <input id='nav-search-bar' class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button id='nav-search-button' class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <Link className='navbar-brand' to='/'>
+          <img 
+            id ="nav-icon" 
+            src="https://cdn-icons-png.flaticon.com/512/52/52053.png"
+            alt=""
+          />
+        </Link>
+
+        {/* <button 
+          type='button' 
+          className="btn btn-light" 
+          id="nav-home"
+        >Home</button>
+
+        <form 
+          id='nav-search' 
+          className="form-inline my-2 my-lg-0"
+        >
+          <input 
+            id='nav-search-bar' className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
+          />
+          <button 
+            id='nav-search-button' 
+            className="btn btn-outline-dark my-2 my-sm-0" type="submit"
+          >Search</button>
+        </form> */}
         
-        <section id='nav-user' >
-          <img id='nav-user-icon' alt='/' src='https://scontent.fhan2-1.fna.fbcdn.net/v/t39.30808-6/276056514_1920086988187184_1368438565368509908_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MNFcLHwFfagAX-SLvfu&tn=tFfAYszXuokIL2RM&_nc_ht=scontent.fhan2-1.fna&oh=00_AT86uvXVpETwdjAY1fF5EqUFgBMofsqDJUVxQoVSj5VNsA&oe=62496663'/>
-          <p id='nav-user-name'> Nguyen Gia Thanh </p>
-          <div class="btn-group" id='nav-dropdown'>
-          <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"/>
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li><button class="dropdown-item" type="button">User Profile</button></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><button class="dropdown-item" type="button">Log Out</button></li>
-          </ul>
-        </div>
-        </section>
-    
         {navbar.isLoading 
         ? <i className='bi bi-arrow-clockwise' id='navbar-user-loading'></i> 
         : <div id='navbar-user'>
@@ -52,6 +59,9 @@ const Navbar = () => {
             <h2 id='navbar-user-name'>{identity.name}</h2>
             <p id='navbar-user-coins'>Coins:{identity.coins}</p>
             <p id='navbar-user-karma'>karma:{identity['total_karma']}</p>
+            <button className='dropdown-trigger'>
+              <i className='bi bi-arrow-down' ></i>
+            </button>
         </div>}
       </div>
     </nav>
