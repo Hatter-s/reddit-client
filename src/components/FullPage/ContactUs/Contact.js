4d74282ce8ef592d425c8afac447924beeb07011
thanhHH1907 <67831088+thanhHH1907@users.noreply.github.com>
2022-04-18 00:30:47 +0700

    update components

--- src/components/FullPage/ContactUs/Contact.js
+++ src/components/FullPage/ContactUs/Contact.js
@@ -6,9 +6,29 @@ import {ContactHeader,ContactTeam,Information} from '../../../features/ContactUs
 function Contact() {
     return (
       <div className='ContactContainer' id="contact">     
-        <ContactHeader/>
-        <ContactTeam/>
-        <Information/>      
+        <h1 id='contact-header'>Contact Us</h1>
+        <div id="contact-team">
+            <p>GTV Team</p>
+            <p>Gnosis To Victory</p>
+        </div>
+        <div id="information">
+                <section id='person'>
+                    <i class="bi bi-person-fill"></i>
+                    <p>Mr.Thanh</p>
+                </section>
+                <section id='phone'>
+                    <i class="bi bi-telephone-fill"></i>
+                    <p>+84 921852309</p>
+                </section>
+                <section id='email'>
+                    <i class="bi bi-envelope-fill"></i>
+                    <p>thanhHH1907@gmail.com</p>
+                </section>
+                <section id='address'>
+                    <i class="bi bi-door-open-fill"></i>
+                    <p>178 Quan Nhan, Thanh Xuan , Ha Noi , VietNam</p>
+                </section>
+        </div>    
       </div>
     );
   }
